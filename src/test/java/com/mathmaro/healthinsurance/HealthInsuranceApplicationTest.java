71c7d4d0a1cbf8873b48b9ba25dc625a90b32051
mathmaro <mathmaro@br.ibm.com>
2019-10-04 16:33:02 -0300

    fixed testing not running with mvn

--- src/test/java/com/mathmaro/healthinsurance/HealthInsuranceApplicationTest.java
+++ src/test/java/com/mathmaro/healthinsurance/HealthInsuranceApplicationTest.java
@@ -1,13 +1,13 @@
 package com.mathmaro.healthinsurance;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HealthInsuranceApplicationTests {
+public class HealthInsuranceApplicationTest {
 
 	@Test
 	public void contextLoads() {
